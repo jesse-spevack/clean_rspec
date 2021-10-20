3e9061e37847bd31fd83fba411aa9e1d75ea8c84
Jesse Spevack <jesse.spevack@ibotta.com>
2021-10-20 15:55:50 -0600

    Passing

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -26,7 +26,7 @@ module GildedRose
       return if @quality >= 50
 
       @quality += 1
-      @quality += 1 if @days_remaining <= 0
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
     end
   end
 
