876cd8d3fadd6c1759cb7207b6b4aeb9ba9c44e7
emrusso <emmi.russo@brandfolder.com>
2021-11-09 13:51:46 -0700

    arrange, act, assert + random other organization

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -9,248 +9,254 @@ RSpec.describe GildedRose do
     expect(gilded_rose).to be_a(GildedRose)
   end
 
-  context "after sell date" do
-    it "decreases quality and days remaining" do
-      gr = GildedRose.new(name: "Normal Item", days_remaining: -10, quality: 10)
+  describe("#tick") do
+    context "after sell date" do
+      let(:gr) { GildedRose.new(name: "Normal Item", days_remaining: -10, quality: 10) }
+
+      it "decreases quality and days remaining" do    
+        gr.tick
+    
+        expect(gr.days_remaining).to eq(-11)
+        expect(gr.quality).to eq(8)
+      end
+    end
+
+    shared_examples :gilded_rose do |name, days_remaining, quality, expected_days_remaining, expected_quality|
+      it 'ticks' do
+        gr = GildedRose.new(name: name, days_remaining: days_remaining, quality: quality)
+        gr.tick
+        expect(gr).to have_attributes(days_remaining: expected_days_remaining, quality: expected_quality)
+      end
+    end
+
+    context 'before sell date' do
+      it "decreases days remaining and quality" do
+        gr = GildedRose.new(name: "Normal Item", days_remaining: 5, quality: 10)
+        gr2 = GildedRose.new(name: "Normal Item", days_remaining: -1, quality: 8)
+        gr3 = GildedRose.new(name: "Normal Item", days_remaining: 1, quality: 12)
   
+        gr.tick
+  
+        expect(gr).to have_attributes(days_remaining: 4, quality: 9)
+      end
+    end
+
+    context 'on sell date' do
+      let(:gr) { GildedRose.new(name: "Normal Item", days_remaining: 0, quality: 10) }
+
+      it "decreases days remaining and quality" do    
+        gr.tick
+  
+        expect(gr.days_remaining).to eq(-1)
+        expect(gr.quality).to eq(8)
+      end
+    end
+
+    context 'normal item of zero quality' do
+      it "decreases days remaining and does not decrease quality below 0" do
+        gr = GildedRose.new(name: name, days_remaining: 5, quality: 0)
+  
+        gr.tick
+  
+        expect(gr.days_remaining).to eq(4)
+        expect(gr.quality).to eq(0)
+      end
+    end
+
+    it_behaves_like :gilded_rose, "Aged Brie", 5, 10, 4, 11
+    it_behaves_like :gilded_rose, "Aged Brie", 5, 50, 4, 50
+    it_behaves_like :gilded_rose, "Aged Brie", 0, 10, -1, 12
+    it_behaves_like :gilded_rose, "Aged Brie", 0, 49, -1, 50
+    it_behaves_like :gilded_rose, "Aged Brie", 0, 50, -1, 50
+    it_behaves_like :gilded_rose, "Aged Brie", -10, 10, -11, 12
+    it_behaves_like :gilded_rose, "Aged Brie", -10, 50, -11, 50
+
+    it "sulfuras before sell date" do
+      gr = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 5, quality: 80)
+
       gr.tick
-  
+
+      expect(gr.days_remaining).to eq(5)
+      expect(gr.quality).to eq(80)
+    end
+
+    it "sulfuras on sell date" do
+      gr = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 0, quality: 80)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(0)
+      expect(gr.quality).to eq(80)
+    end
+
+    it "sulfuras after sell date" do
+      gr = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: -10, quality: 80)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(-10)
+      expect(gr.quality).to eq(80)
+    end
+
+    it "backstage passes long before sell date" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 10)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(10)
+      expect(gr.quality).to eq(11)
+    end
+
+    it "backstage passes long before sell date at max quality" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 50)
+
+      gr.tick
+
+      expect(gr).to have_attributes(days_remaining: 10, quality: 50)
+    end
+
+    it "backstage passes medium close to sell date upper bound" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 10)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(9)
+      expect(gr.quality).to eq(12)
+    end
+
+    it "backstage passes medium close to sell date upper bound at max quality" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 50)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(9)
+      expect(gr.quality).to eq(50)
+    end
+
+    it "backstage passes medium close to sell date lower bound" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 10)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(5)
+      expect(gr.quality).to eq(12)
+    end
+
+    it "backstage passes medium close to sell date lower bound at max quality" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 50)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(5)
+      expect(gr.quality).to eq(50)
+    end
+
+    it "backstage passes very close to sell date upper bound" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 10)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(4)
+      expect(gr.quality).to eq(13)
+    end
+
+    it "backstage passes very close to sell date upper bound at max quality" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 50)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(4)
+      expect(gr.quality).to eq(50)
+    end
+
+    it "backstage passes very close to sell date lower bound" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 10)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(0)
+      expect(gr.quality).to eq(13)
+    end
+
+    it "backstage passes very close to sell date lower bound at max quality" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 50)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(0)
+      expect(gr.quality).to eq(50)
+    end
+
+    it "backstage passes on sell date" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 0, quality: 10)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(-1)
+      expect(gr.quality).to eq(0)
+    end
+
+    it "backstage passes after sell date" do
+      gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: -10, quality: 10)
+  # 
+      gr.tick
+
       expect(gr.days_remaining).to eq(-11)
+      expect(gr.quality).to eq(0)
+    end
+
+    xit "conjured mana before sell date" do
+      gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 10)
+
+      gr.tick
+
+      expect(gr.days_remaining).to eq(4)
       expect(gr.quality).to eq(8)
     end
-  end
 
-  shared_examples :gilded_rose do |name, days_remaining, quality, expected_days_remaining, expected_quality|
-    it 'ticks' do
-      gr = GildedRose.new(name: name, days_remaining: days_remaining, quality: quality)
+    xit "conjured mana before sell date at zero quality" do
+      gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 0)
+
       gr.tick
-      expect(gr).to have_attributes(days_remaining: expected_days_remaining, quality: expected_quality)
+
+      expect(gr.days_remaining).to eq(4)
+      expect(gr.quality).to eq(0)
     end
-  end
 
-  it "normal item before sell date" do
-    gr = GildedRose.new(name: "Normal Item", days_remaining: 5, quality: 10)
-    gr2 = GildedRose.new(name: "Normal Item", days_remaining: -1, quality: 8)
-    gr3 = GildedRose.new(name: "Normal Item", days_remaining: 1, quality: 12)
+    xit "conjured mana on sell date" do
+      gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 10)
 
-    gr.tick
+      gr.tick
 
-    expect(gr).to have_attributes(days_remaining: 4, quality: 9)
-  end
+      expect(gr.days_remaining).to eq(-1)
+      expect(gr.quality).to eq(6)
+    end
 
-  it "normal item on sell date" do
-    gr = GildedRose.new(name: "Normal Item", days_remaining: 0, quality: 10)
+    xit "conjured mana on sell date at zero quality" do
+      gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 0)
 
-    expect(gr).to be_instance_of(GildedRose) 
+      gr.tick
 
-    gr.tick
+      expect(gr.days_remaining).to eq(-1)
+      expect(gr.quality).to eq(0)
+    end
 
-    expect(gr.days_remaining).to eq(-1)
-    expect(gr.quality).to eq(8)
-  end
+    xit "conjured mana after sell date" do
+      gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 10)
 
-  it "normal item of zero quality" do
-    gr = GildedRose.new(name: name, days_remaining: 5, quality: 0)
+      gr.tick
 
-    gr.tick
+      expect(gr.days_remaining).to eq(-11)
+      expect(gr.quality).to eq(6)
+    end
 
-    expect(gr.days_remaining).to eq(4)
-    expect(gr.quality).to eq(0)
-  end
+    xit "conjured mana after sell date at zero quality" do
+      gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 0)
 
-  it_behaves_like :gilded_rose, "Aged Brie", 5, 10, 4, 11
-  it_behaves_like :gilded_rose, "Aged Brie", 5, 50, 4, 50
-  it_behaves_like :gilded_rose, "Aged Brie", 0, 10, -1, 12
-  it_behaves_like :gilded_rose, "Aged Brie", 0, 49, -1, 50
-  it_behaves_like :gilded_rose, "Aged Brie", 0, 50, -1, 50
-  it_behaves_like :gilded_rose, "Aged Brie", -10, 10, -11, 12
-  it_behaves_like :gilded_rose, "Aged Brie", -10, 50, -11, 50
+      gr.tick
 
-  it "sulfuras before sell date" do
-    gr = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 5, quality: 80)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(5)
-    expect(gr.quality).to eq(80)
-  end
-
-  it "sulfuras on sell date" do
-    gr = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 0, quality: 80)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(0)
-    expect(gr.quality).to eq(80)
-  end
-
-  it "sulfuras after sell date" do
-    gr = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: -10, quality: 80)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(-10)
-    expect(gr.quality).to eq(80)
-  end
-
-  it "backstage passes long before sell date" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 10)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(10)
-    expect(gr.quality).to eq(11)
-  end
-
-  it "backstage passes long before sell date at max quality" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 50)
-
-    gr.tick
-
-    expect(gr).to have_attributes(days_remaining: 10, quality: 50)
-  end
-
-  it "backstage passes medium close to sell date upper bound" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 10)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(9)
-    expect(gr.quality).to eq(12)
-  end
-
-  it "backstage passes medium close to sell date upper bound at max quality" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 50)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(9)
-    expect(gr.quality).to eq(50)
-  end
-
-  it "backstage passes medium close to sell date lower bound" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 10)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(5)
-    expect(gr.quality).to eq(12)
-  end
-
-  it "backstage passes medium close to sell date lower bound at max quality" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 50)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(5)
-    expect(gr.quality).to eq(50)
-  end
-
-  it "backstage passes very close to sell date upper bound" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 10)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(4)
-    expect(gr.quality).to eq(13)
-  end
-
-  it "backstage passes very close to sell date upper bound at max quality" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 50)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(4)
-    expect(gr.quality).to eq(50)
-  end
-
-  it "backstage passes very close to sell date lower bound" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 10)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(0)
-    expect(gr.quality).to eq(13)
-  end
-
-  it "backstage passes very close to sell date lower bound at max quality" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 50)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(0)
-    expect(gr.quality).to eq(50)
-  end
-
-  it "backstage passes on sell date" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 0, quality: 10)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(-1)
-    expect(gr.quality).to eq(0)
-  end
-
-  it "backstage passes after sell date" do
-    gr = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: -10, quality: 10)
-# 
-    gr.tick
-
-    expect(gr.days_remaining).to eq(-11)
-    expect(gr.quality).to eq(0)
-  end
-
-  xit "conjured mana before sell date" do
-    gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 10)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(4)
-    expect(gr.quality).to eq(8)
-  end
-
-  xit "conjured mana before sell date at zero quality" do
-    gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 0)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(4)
-    expect(gr.quality).to eq(0)
-  end
-
-  xit "conjured mana on sell date" do
-    gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 10)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(-1)
-    expect(gr.quality).to eq(6)
-  end
-
-  xit "conjured mana on sell date at zero quality" do
-    gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 0)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(-1)
-    expect(gr.quality).to eq(0)
-  end
-
-  xit "conjured mana after sell date" do
-    gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 10)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(-11)
-    expect(gr.quality).to eq(6)
-  end
-
-  xit "conjured mana after sell date at zero quality" do
-    gr = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 0)
-
-    gr.tick
-
-    expect(gr.days_remaining).to eq(-11)
-    expect(gr.quality).to eq(0)
+      expect(gr.days_remaining).to eq(-11)
+      expect(gr.quality).to eq(0)
+    end
   end
 end
